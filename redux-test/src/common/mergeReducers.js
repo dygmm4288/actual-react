6d8b4283a62934bd377faa2b5ef29e9bb69c8824
easyno <dlwlsghgk124@naver.com>
2020-01-21 15:32:16 +0900

    use redux package

---
+++ redux-test/src/common/mergeReducers.js
@@ -0,0 +1,16 @@
+export default function mergeReducers(reducers) {
+	return function(state, action) {
+		if (!state) {
+			return reducers.reduce(
+				(acc, r) => ({ ...acc, ...r(state, action) }),
+				{}
+			);
+		} else {
+			let nextState = state;
+			for (const r of reducers) {
+				nextState = r(nextState, action);
+			}
+			return nextState;
+		}
+	};
+}
